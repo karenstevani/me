adaa8abbf0f5e01a6c9709d3c2b16f5fa026c8b1
Karen Stevani <karenstevanixiao@gmail.com>
2024-06-05 18:13:31 +1000

    exercise 1 week 2

--- set2/exercise1.py
+++ set2/exercise1.py
@@ -17,27 +17,32 @@ import platform
 # I think this will print "hello! Let's get started" by calling the print function.
 print("hello! Let's get started")  # it printed "hello! Let's get started"
 
-some_words = ["what", "does", "this", "line", "do", "?"]
+# I think this will assign the list to the variable some_words
+some_words = ["what", "does", "this", "line", "do", "?"] # it assigned the list into variable some_words
 
-for word in some_words:
-    print(word)
+# I think this will create a loop that reads and calls each item of the some_words list and print that word
+for word in some_words: 
+    print(word) # it printed "what does this line do ?" with the format of one word per line
 
+# I think this is the same as above and will create a loop that reads and calls each item of the some_words list and print the items
 for x in some_words:
-    print(x)
+    print(x) # it printed "what does this line do?" with the format of one word per line
 
-print(some_words)
+# I think this will print all the items in the list some_words by calling the print function 
+print(some_words) # it printed "['what', 'does', 'this', 'line', 'do', '?']"
 
+# I think this will check if the amount of items in the some_words list is greater than 3, and if so it'll print the phrase by calling the print function
 if len(some_words) > 3:
-    print("some_words contains more than 3 words")
+    print("some_words contains more than 3 words") # it printed "some_words contains more than 3 words"
 
-
+# I think this will define the function usefulFunction() as printing the name, operating system, version, and other details of the computer it's running on
 def usefulFunction():
     """
     You may want to look up what uname does before you guess
     what the line below does:
     https://docs.python.org/3/library/platform.html#platform.uname
     """
-    print(platform.uname())
+    print(platform.uname()) # it printed "uname_result(system='Windows', node='Karen', release='11', version='10.0.22631', machine='AMD64')"
 
 
 usefulFunction()

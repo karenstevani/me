7e18631ee20132b48ba518ea1f370511c7846a3f
Karen Stevani <karenstevanixiao@gmail.com>
2024-06-28 21:45:33 +1000

    set 4 trial

--- set4/exercise1.py
+++ set4/exercise1.py
@@ -40,7 +40,10 @@ def get_some_details():
     json_data = open(LOCAL + "/lazyduck.json").read()
 
     data = json.loads(json_data)
-    return {"lastName": None, "password": None, "postcodePlusID": None}
+    lastName = data["results"][0]["name"]["last"]
+    password = data["results"][0]["login"]["password"]
+    postcodePlusID = data["results"][0]["location"]["postcode"] + data["results"][0]["id"]
+    return {"lastName": lastName, "password": password, "postcodePlusID": postcodePlusID}
 
 
 def wordy_pyramid():

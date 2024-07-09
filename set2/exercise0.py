21cf5bd4b592d02720975587ed90e34af0541cf2
Karen Stevani <karenstevanixiao@gmail.com>
2024-07-09 12:35:26 +1000

    f string update

--- set2/exercise0.py
+++ set2/exercise0.py
@@ -81,7 +81,7 @@ def really_shout(a_string):
     that you've reused the function you already wrote.
     Look up how to 'concatinate' strings to make this happen.
     """
-    the_answer = shout(a_string) + "!"
+    the_answer = f"{shout(a_string)}!"
     return the_answer
 
 

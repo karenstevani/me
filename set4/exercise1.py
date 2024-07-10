b446c370b2d61e8bde9099ee0c6b08a035f95dbc
Karen Stevani <karenstevanixiao@gmail.com>
2024-07-10 15:59:01 +1000

    set 4 update

--- set4/exercise1.py
+++ set4/exercise1.py
@@ -84,15 +84,15 @@ def wordy_pyramid():
     """
     pyramid = []
 
-    for i in range (3, 10, 2):
-        url = f"https://us-central1-waldenpondpress.cloudfunctions.net/give_me_a_word?wordlength=20"
+    for i in range (3, 20, 2):
+        url = f"https://us-central1-waldenpondpress.cloudfunctions.net/give_me_a_word?wordlength={i}"
         response = requests.get(url)
         if response.status_code == 200:
             word = response.text
             pyramid.append(word)
 
-    for i in range(11, 4, -2):
-        url = f"https://us-central1-waldenpondpress.cloudfunctions.net/give_me_a_word?wordlength=20"
+    for i in range(20, 3, -2):
+        url = f"https://us-central1-waldenpondpress.cloudfunctions.net/give_me_a_word?wordlength={i}"
         response = requests.get(url)
         if response.status_code == 200: 
             word = response.text
@@ -115,13 +115,22 @@ def pokedex(low=1, high=5):
          get very long. If you are accessing a thing often, assign it to a
          variable and then future access will be easier.
     """
-    id = 5
-    url = f"https://pokeapi.co/api/v2/pokemon/{id}"
-    r = requests.get(url)
-    if r.status_code is 200:
-        the_json = json.loads(r.text)
+    id = low
+    tallest = 0
+    while id < high:
+        url = f"https://pokeapi.co/api/v2/pokemon/{id}"
+        r = requests.get(url)
+        if r.status_code is 200:
+            url = json.loads(r.text)
+            height = url["height"]
+            if tallest < height:
+                name = url["forms"][0]["name"]
+                weight = url["weight"]
+                tallest = url["height"]
 
-    return {"name": None, "weight": None, "height": None}
+        id = id + 1
+
+    return {"name": name, "weight": weight, "height": tallest}
 
 
 def diarist():
